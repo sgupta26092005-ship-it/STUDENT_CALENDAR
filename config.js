c62b55e36b008bc20ee44f27f19341cb023f9ec5
sgupta26092005-ship-it <sgupta26092005@gmail.com>
2026-04-06 10:32:21 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration
-export const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "your-app-id"
-};
